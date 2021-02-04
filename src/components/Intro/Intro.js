052aa65cb2170e5709ff08a02e121300e28efef2
manwillaaron <manwillaaron@gmail.com>
2021-02-04 11:08:06 -0700

    added typing and deleting

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,7 +1,30 @@
 import './Intro.css'
+import me from '../../assets/me.png'
+import Typing from 'react-typing-animation'
 
-const Intro =()=>(
-    <div className='intro-container'>Intro</div>
+const AnimatedTypingComponent = () => (
+  <Typing startDelay={1000}>
+    <span className='intro'>movie lover</span>
+    <Typing.Delay ms={1600} />
+    <Typing.Backspace count={15} />
+    <span className='intro'>dad of three</span>
+    <Typing.Delay ms={1600} />
+    <Typing.Backspace count={15} />
+    <span className='intro'>video gamer</span>
+    <Typing.Delay ms={1600} />
+    <Typing.Backspace count={15} />
+    <span className='intro' style={{textDecoration: 'underline'}}>web developer</span>
+    <Typing.Delay ms={1000000000000}/>
+  </Typing>
+);
+
+
+const Intro = () => (
+    <div className='intro-container'>
+        <h1>Hello! my name is Aaron Manwill</h1>
+        <p>I am a&#xA0;</p><p><AnimatedTypingComponent /></p>
+        <img src={me} alt=''/>
+    </div>
 )
 
 export default Intro
